22fe1bc67f07eb0b066237419674201a264acbe9
OmarNizam <eng.omarnizam@gmail.com>
2026-08-29 11:09:38 +0200

    make JpaRepository<SoftwareEngineer, UUID> while the entitys @Id is now UUID

--- src/main/java/com/amigoscode/SoftwareEngineerRepository.java
+++ src/main/java/com/amigoscode/SoftwareEngineerRepository.java
@@ -2,5 +2,7 @@ package com.amigoscode;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+import java.util.UUID;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, UUID> {
 }
